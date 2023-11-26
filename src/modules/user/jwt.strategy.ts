ac2e5beadd89e503f8c6f8c896b393c1b81ad6d9
Dmitry <dimkap314@gmail.com>
2023-11-26 15:05:59 +0300

    fix

--- src/modules/user/jwt.strategy.ts
+++ src/modules/user/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secret',
+      secretOrKey: jwtConstants.secret,
     });
   }
 
