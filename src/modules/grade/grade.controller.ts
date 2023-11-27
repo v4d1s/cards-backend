c0474b93ec6e066ac4b9f58f53d8bf6c4939e298
Vadim Kulikov <kylukov2003@yandex.ru>
2023-11-27 18:56:49 +0300

    Fix route

--- src/modules/grade/grade.controller.ts
+++ src/modules/grade/grade.controller.ts
@@ -2,11 +2,11 @@ import {Body, Controller, Patch} from '@nestjs/common';
 import {GradeService} from "./grade.service";
 import {CreateGradeDTO} from "./dto";
 
-@Controller('/pack/:packId/:cardId')
+@Controller('/pack/:packId/card/:cardId')
 export class GradeController {
     constructor(private readonly gradeService: GradeService) {}
 
-    @Patch()
+    @Patch('grade')
     updateGrade(@Body() dto: CreateGradeDTO) {
         return this.gradeService.updateGrade(dto);
     }
